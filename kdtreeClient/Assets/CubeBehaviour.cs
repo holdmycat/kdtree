0f40ae58d82da94979b7d8992255ec42fb66028f
root <domizxf@gmail.com>
2020-08-25 11:49:27 +0800

    添加kdtree连线

---
+++ kdtreeClient/Assets/CubeBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour
+{
+
+    public LineRenderer lineLeftRender;
+    public LineRenderer lineRightRender;
+
+
+    public Material Mat;
+
+    private void Start()
+    {
+        Mat = GetComponent<MeshRenderer>().materials[0];
+        //lineRender = GetComponent<LineRenderer>();
+    }
+
+   public void SetParent()
+   {
+        // lineRender.SetPositions(new Vector3[] { transform.position, point});
+        Mat.SetColor("_Color",Color.red);
+   }
+   public void SetLeft(Vector3 point)
+   {
+        lineLeftRender.materials[0].SetColor("_Color", Color.green);
+        lineLeftRender.SetPositions(new Vector3[] { transform.position, point });
+   }
+   public void SetRight(Vector3 point)
+   {
+        lineRightRender.materials[0].SetColor("_Color", Color.blue);
+        lineRightRender.SetPositions(new Vector3[] { transform.position, point });
+   }
+
+}
---
+++ kdtreeClient/Assets/TestKdTree.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestKdTree : MonoBehaviour
+{
+    
+    Vector2[] _mArrayData = new Vector2[] {
+        new Vector2(-3,6),
+        new Vector2(-6,2),
+        new Vector2(0,5),
+        new Vector2(1,-2),
+        new Vector2(7,-4),
+        new Vector2(5,3)
+    };
+
+    KdTree<CubeBehaviour> _mKdList = new KdTree<CubeBehaviour>();
+
+    Queue _mQueue = new Queue();
+
+    public CubeBehaviour _mOrigCubeBeha;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        for(var i = 0; i < _mArrayData.Length; i++)
+        {
+            var tmp = Instantiate(_mOrigCubeBeha.gameObject, _mArrayData[i], Quaternion.identity);
+            _mQueue.Enqueue(tmp.GetComponent<CubeBehaviour>());
+        }
+
+    }
+
+    private void OnGUI()
+    {
+        if(GUI.Button(new Rect(0,0,150,150), "kd item"))
+        {
+            CubeBehaviour tmp = _mQueue.Dequeue() as CubeBehaviour;
+            _mKdList.Add(tmp);
+        }
+    }
+
+}
